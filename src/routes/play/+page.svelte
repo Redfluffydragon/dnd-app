76908169dc5bf7617540073a0e85f899feb2a2e6
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-14 22:43:25 -0700

    Remove unused websocket code

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -5,29 +5,9 @@
   // export let data;
 
   onMount(() => {
-    const socket = new WebSocket(`ws://${location.host}/play`);
-
-    // Connection opened
-    socket.addEventListener("open", (event) => {
-      socket.send("Hello Server!");
-    });
-
     let left = 0;
 
     // Listen for messages
-    socket.addEventListener("message", (event) => {
-      console.log("Message from server:", event.data);
-      const lr = event.data.match(/(left|right)/)?.[0];
-      if (lr) {
-        if (lr === "left") {
-          left -= 10;
-        } else {
-          left += 10;
-        }
-        document.getElementById("testToken").style.left = left + "px";
-      }
-    });
-
     ipc.on('control', (e, msg) => {
       console.log(msg);
       const lr = msg.match(/(left|right)/)?.[0];
