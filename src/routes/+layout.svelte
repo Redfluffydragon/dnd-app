795c2f1426cd8225f41c92c266d4cc4771e9d3c5
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-15 13:08:00 -0700

    Add header with a home link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,19 @@
   import '../app.scss';
 </script>
 
+<header>
+  <a href="/">Home</a>
+</header>
+
 <main>
   <slot />
 </main>
 
 <style>
+  header {
+    padding: 1ch;
+  }
+
   main {
     padding: 10px;
   }
