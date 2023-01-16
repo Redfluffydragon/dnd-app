da61ff3ba9c8600ec28e63c0ee3bf88625fdd472
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-15 17:03:15 -0700

    Add Column component

---
+++ src/lib/Column.svelte
@@ -0,0 +1,15 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-template-columns: 1fr calc(100% - 44px) 1fr;
+    place-items: center;
+  }
+
+  :global(div > *) {
+    grid-column: 2;
+  }
+</style>
