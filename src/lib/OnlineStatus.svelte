6b7859cf1b50138ce96e27b41813a211691c0b45
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-17 17:51:04 -0700

    Add OnlineStatus component to display online status

---
+++ src/lib/OnlineStatus.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let status;
+</script>
+
+<span class:online={status}></span>
+
+<style>
+  span {
+    display: inline-block;
+    width: 1em;
+    border: 2px solid var(--primary-text);
+    aspect-ratio: 1;
+    border-radius: 999px;
+    background: red;
+  }
+
+  .online {
+    background: green;
+  }
+</style>
