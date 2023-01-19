2e569f5b5f2aa14b2a6178be4b66aa92a40ee9ea
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-18 21:41:57 -0700

    Add change global name form (nothing happens in main yet)

--- src/lib/controller/Menu.svelte
+++ src/lib/controller/Menu.svelte
@@ -1,8 +1,48 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
+  import Dropdown from '../Dropdown.svelte';
   import MenuWrapper from '../MenuWrapper.svelte';
+
+  const dispatch = createEventDispatcher();
 </script>
 
 <MenuWrapper>
-  <li><button>Change global name</button></li>
+  <li>
+    <Dropdown title="Change global name">
+      <form
+        on:submit={(e) => {
+          const name = new FormData(e.target).get('name');
+          if (!name) return;
+          e.preventDefault();
+          dispatch('submit', {
+            type: 'changeglobalname',
+            name,
+          });
+        }}
+      >
+        <label>New name:<input type="text" name="name" required /></label>
+        <button>Submit</button>
+      </form>
+    </Dropdown>
+  </li>
   <li><button>Change session name</button></li>
 </MenuWrapper>
+
+<style>
+  form {
+    padding: 2ch;
+  }
+
+  form button {
+    text-align: center;
+    background: var(--secondary);
+  }
+
+  label {
+    text-align: center;
+  }
+
+  input {
+    width: 100%;
+  }
+</style>
