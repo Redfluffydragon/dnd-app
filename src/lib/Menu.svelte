11a3e07c0ace02e09a073ab942429e8d7930f166
Kai-Shen Deru <kderu1420@gmail.com>
2023-01-19 17:55:58 -0700

    Add menu pin functionality

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,16 +1,24 @@
 <script>
   import { fly } from 'svelte/transition';
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
+  import { browser } from '$app/environment';
 
   export let showMenu = false;
-  export let inApp = true;
+  export let pinned = false;
+
+  function hideMenu(e) {
+    if (!pinned && !e.target.closest('menu,.menuButton')) {
+      showMenu = false;
+    }
+  }
 
   onMount(() => {
-    document.addEventListener('click', (e) => {
-      if (!e.target.closest('menu,.menuButton')) {
-        showMenu = false;
-      }
-    });
+    document.addEventListener('click', hideMenu);
+  });
+
+  onDestroy(() => {
+    if (!browser) return;
+    document.removeEventListener('click', hideMenu);
   });
 </script>
 
@@ -30,16 +38,42 @@
   </slot>
 </button>
 {#if showMenu}
-  <menu transition:fly={{ x: -250 }} class:outOfApp={!inApp}>
-    <button
-      class="closeButton imgButton"
-      on:click={() => {
-        showMenu = false;
-      }}
-      ><svg width="52" height="52" viewBox="0 0 52 52" fill="none">
-        <path d="M50 2L2 50M2 2L50 50" stroke="currentColor" stroke-width="5" />
-      </svg>
-    </button>
+  <menu transition:fly={{ x: -250 }}>
+    <div class="buttons">
+      <button
+        class="closeButton imgButton"
+        on:click={() => {
+          showMenu = false;
+          pinned = false;
+        }}
+        ><svg width="52" height="52" viewBox="0 0 52 52" fill="none">
+          <path
+            d="M50 2L2 50M2 2L50 50"
+            stroke="currentColor"
+            stroke-width="5"
+          />
+        </svg>
+      </button>
+      <button
+        class="imgButton"
+        on:click={() => {
+          pinned = !pinned;
+        }}
+      >
+        <svg
+          width="64"
+          height="64"
+          viewBox="0 0 64 64"
+          fill={pinned ? 'currentColor' : 'none'}
+        >
+          <path
+            d="M27.1987 35.3883L15.2621 25.0957C19.4731 21.5913 24.1422 22.8425 25.9504 23.9061L42.0504 9.54448C42.2505 7.9332 42.492 4.72238 43.2272 3.86977L58.5742 17.1031C57.839 17.9557 54.6986 18.6669 53.1343 19.1019L41.2974 37.1394C42.6154 38.7715 44.5398 43.2057 41.693 47.8864L29.7565 37.5938M27.1987 35.3883L13.5978 51.1616L10.8332 56.9537L16.1556 53.3671L29.7565 37.5938M27.1987 35.3883L29.7565 37.5938"
+            stroke="currentColor"
+            stroke-width="5"
+          />
+        </svg>
+      </button>
+    </div>
     <slot />
   </menu>
 {/if}
